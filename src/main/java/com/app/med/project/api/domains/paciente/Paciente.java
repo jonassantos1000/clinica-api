3201a87334f3354a93820f8dccc74ebdab8944af
jonassantos1000 <jonasvale3@hotmail.com>
2023-04-17 13:06:22 -0300

    db: gerado nova versão do migration para tabela de pacientes

--- src/main/java/com/app/med/project/api/domains/paciente/Paciente.java
+++ src/main/java/com/app/med/project/api/domains/paciente/Paciente.java
@@ -2,6 +2,7 @@ package com.app.med.project.api.domains.paciente;
 
 import com.app.med.project.api.domains.endereco.Endereco;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,8 @@ public class Paciente {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
+	
+	@Embedded
 	private Endereco endereco;
 	private String email;
 	private String telefone;
